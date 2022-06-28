a0b36dc34aa866a2a4b016fd175720caf9756743
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-06-29 00:34:17 +0600

    prepend

--- Linked List/Implement Linket List.js
+++ Linked List/Implement Linket List.js
@@ -7,6 +7,7 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
   append(value) {
     const newNode = {
       value: value,
@@ -15,10 +16,20 @@ class LinkedList {
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = { value: value, next: null };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
   }
 }
 
 const Mylinkedlist = new LinkedList(10);
 Mylinkedlist.append(5);
 Mylinkedlist.append(20);
+Mylinkedlist.prepend(1);
 console.log(Mylinkedlist);
