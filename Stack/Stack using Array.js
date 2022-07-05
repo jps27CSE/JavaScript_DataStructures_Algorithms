05f46f66acdfc8d2d0608bbb9b4b1e0b84afaad3
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-07-05 15:50:41 +0600

    Queue

---
+++ Stack/Stack using Array.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.array = [];
+  }
+
+  peek() {
+    return this.array[this.array.length - 1];
+  }
+
+  push(value) {
+    this.array.push(value);
+    return this;
+  }
+
+  pop() {
+    this.array.pop();
+    return this;
+  }
+}
+
+const myStack = new Stack();
+myStack.push(3);
+console.log(myStack.push(2));
+console.log(myStack.pop());
